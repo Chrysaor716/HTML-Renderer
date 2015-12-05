9df69ffbbe99bbaa6b4c03739d21b5f394dd2d3f
Christina Nguyen <phuonghien716@gmail.com>
2015-12-05 01:58:17 -0500

    Added pretty label.

--- HTML-Renderer.py
+++ HTML-Renderer.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 
 import Tkinter
+from Tkinter import Label
 
+# Tkinter.Tk: Base class to inherit from for standard windows.
 base = Tkinter.Tk()
+
+title = Label(base,
+			text="HTML Renderer",
+			font="Times 20 bold",
+			fg="white",
+			bg="black")
+title.pack()	# Fit the size of the window to text
+
+# Opens/Runs the GUI
 base.mainloop()
