b85927421b85a586e1d2458e36e03cc5955bd2a3
Christina Nguyen <phuonghien716@gmail.com>
2015-12-06 00:22:11 -0500

    Scrollbar added for user input text box.

--- HTML-Renderer.py
+++ HTML-Renderer.py
@@ -3,12 +3,16 @@
 import Tkinter
 from Tkinter import Label
 from Tkinter import Text
+from Tkinter import Scrollbar
 
 # Tkinter.Tk: Base class to inherit from for standard windows.
 base = Tkinter.Tk()
 
 """		Title 		"""
 base.title("HTML Renderer")
+
+"""		Scrollbar		"""
+scrollbar = Scrollbar(base)
 
 """		Window Label		"""
 title = Label(base,
@@ -25,13 +29,15 @@ usrInput = Text(base,
 			fg="black",
 			bg="white")
 usrInput.pack(fill="y", padx=10, side="left")
+scrollbar.pack(fill="y", side="left")
+scrollbar.config(command=usrInput.yview)
+usrInput.config(yscrollcommand=scrollbar.set)
 
 """		Message Box to Reflect User Inputs		"""
 output = Text(base,
 			font="Times 14 bold",
 			fg="black",
 			bg="white")
-#output.insert(index[,string])
 output.pack(fill="y", padx=10, side="right")
 
 # Display the window
