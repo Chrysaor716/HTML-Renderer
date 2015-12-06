708d9c7e86c5797907625e8c51bd4edff49c293e
Christina Nguyen <phuonghien716@gmail.com>
2015-12-06 18:17:30 -0500

    Changed to key release event & single quotes.

--- HTML-Renderer.py
+++ HTML-Renderer.py
@@ -3,8 +3,10 @@
 import Tkinter
 from Tkinter import Label, Text, Scrollbar
 
-def textChanged(event=None):
-	print 'text changed!'
+"""		Event handler for key release events
+"""
+def onKeyReleased(event):
+	print 'key released: ' + event.char
 
 # Tkinter.Tk: Base class to inherit from for standard windows.
 base = Tkinter.Tk()
@@ -16,36 +18,36 @@ base.title("HTML Renderer")
 """		Window Label
 """
 title = Label(base,
-			text="A more interactive HTML coding environment!",
-			font="Times 10 bold",
-			fg="white",		# Text color
-			bg="black",		# Text background color
-			anchor="center")
+			text='A more interactive HTML coding environment!',
+			font='Times 10 bold',
+			fg='white',		# Text color
+			bg='black',		# Text background color
+			anchor='center')
 title.pack()	# Fit the size of the window to text
 
 """		Text Box For User Input
 """
 usrInput = Text(base,
-			font="Times 14",
-			fg="black",
-			bg="white")
-usrInput.pack(fill="y", padx=10, side="left")
+			font='Times 14',
+			fg='black',
+			bg='white')
+usrInput.pack(fill='y', padx=10, side='left')
 usrScroll = Scrollbar(base)
-usrScroll.pack(fill="y", side="left")
+usrScroll.pack(fill='y', side='left')
 usrScroll.config(command=usrInput.yview)
 usrInput.config(yscrollcommand=usrScroll.set)
-# Bind the Tkinter <<Modified>> virtual event to callback
-usrInput.bind('<<Modified>>', textChanged)
+# Bind user input text box to key release event
+usrInput.bind('<KeyRelease>', onKeyReleased)
 
 """		Message Box to Reflect User Inputs
 """
 output = Text(base,
-			font="Times 14 bold",
-			fg="black",
-			bg="white")
+			font='Times 14 bold',
+			fg='black',
+			bg='white')
 outScroll = Scrollbar(base)
-outScroll.pack(fill="y", side="right")
-output.pack(fill="y", padx=10, side="right")
+outScroll.pack(fill='y', side='right')
+output.pack(fill='y', padx=10, side='right')
 outScroll.config(command=output.yview)
 output.config(yscrollcommand=outScroll.set)
 
