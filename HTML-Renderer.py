0c04ecfeae7fd8bab53480d525966736a325cd1d
Christina Nguyen <phuonghien716@gmail.com>
2015-12-06 21:58:08 -0500

    Buggy, commented corner case + failed tag usage.

--- HTML-Renderer.py
+++ HTML-Renderer.py
@@ -10,7 +10,8 @@ DEFAULT_FONT_SIZE = 14
 """
 # ThisisprobablysuperinefficientI'msorry #cringe
 def onKeyReleased(event):
-	indexSnap = 0
+	tagStart = 0	# Start of index for syntax highlighting
+#	insertChar = 1	# Boolean to prevent insertting HTML tags
 	# Clear output text box every time script runs this frame
 	output.delete('1.0', 'end')
 	
@@ -18,25 +19,43 @@ def onKeyReleased(event):
 	text = usrInput.get('1.0', 'end')
 	# Look for important characters and parse
 	for index, char in enumerate(text):
-		if char == '>':
-			# See if it's the opening or closing tag
-			if text[index-3] == '/': # If closing tag
-				# No switch/case in Python :(
-				if text[index-1] == '1': # Header 1
-					print 'font 32pt'
-				elif text[index-1] == '2': # Header 2
-					print '24pt'
-				elif text[index-1] == '3': # Header 3
-					print '19pt'
-				elif text[index-1] == '4': # Header 4
-					print '16pt'
-				elif text[index-1] == '5': # Header 5
-					print '13pt'
-				elif text[index-1] == '6': # Header 6
-					print '10pt'
-				else: 			   # Invalid character
-					print 'Do nothing; keep at default font size'
-	
+#		# Check whether or not user is typing an HTML tag
+#		if char == '>':
+#			if text[index-2] == 'h': # Header tags have an extra char
+#				if text[index-3] == '<' or text[index-3] == '/':
+#					insertChar = 0
+#			elif text[index-1] == 'p': # Paragraph tag
+#				if text[index-2] == '<' or text[index-2] == '/':
+#					insertChar = 0
+#			else:
+#				insertChar = 1
+
+		if text[index-1] == '>':
+			# Check if it's a header tag
+			if text[index-3] == 'h':
+				# See if it's the opening or closing tag
+				if text[index-4] == '/': # If closing tag
+					# No switch/case in Python :(
+					if text[index-2] == '1': # Header 1
+						print 'font 32pt'
+#						output.tag_config('h1', font='Times 32 bold')
+#						output.tag_add('h1', 'tagStart', 'index')
+					elif text[index-2] == '2': # Header 2
+						print '24pt'
+					elif text[index-2] == '3': # Header 3
+						print '19pt'
+					elif text[index-2] == '4': # Header 4
+						print '16pt'
+					elif text[index-2] == '5': # Header 5
+						print '13pt'
+					elif text[index-2] == '6': # Header 6
+						print '10pt'
+					else: 			   # Invalid character
+						print 'Subliminal messaging.'
+				#if text[index-4] == '<': # Opening tag
+				#	tagStart = index
+
+#		if insertChar == 1:
 		output.insert('end', char)
 
 # Tkinter.Tk: Base class to inherit from for standard windows.
@@ -49,7 +68,7 @@ base.title("HTML Renderer")
 """		Window Label
 """
 title = Label(base,
-			text='A more interactive HTML coding environment!',
+			text='A more interactive HTML coding environment!\nPlease type your code in the left text box.',
 			font='Times 10 bold',
 			fg='white',		# Text color
 			bg='black',		# Text background color
